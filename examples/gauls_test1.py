f1864e99a25d2fda88bbe422b33398e8cff11896
lukepinkel <lukepinkel@gmail.com>
2021-04-25 12:25:33 -0700

    Update gauls_test1.py

--- examples/gauls_test1.py
+++ examples/gauls_test1.py
@@ -45,3 +45,36 @@ np.allclose(np.array([4.235378, 4.165951, 7.181457]), mod.theta)
 
 print(mod.res)
 print(mod.res_smooths)
+
+         
+n_obs = 50_000
+df = pd.DataFrame(np.zeros((n_obs, 4)), columns=['x0', 'x1', 'x2', 'y'])
+
+df['x0'] = rng.choice(np.arange(5), size=n_obs, p=np.ones(5)/5)
+df['x1'] = rng.uniform(-1, 1, size=n_obs)
+df['x2'] = rng.uniform(-1, 1, size=n_obs)
+
+u0 =  dummy(df['x0']).dot(np.array([-0.2, 0.2, -0.2, 0.2, 0.0]))
+f1 = (4.0 * df['x1']**3 - 4.0 * df['x1'])
+f2 = -(4.0 * df['x2']**3 - 4.0 * df['x2']) 
+f3 = (df['x2'] - 1.0) * (df['x2'] + 1.0)
+eta =  u0 + f1 + f2
+mu = eta.copy() 
+tau = 1.0 / (np.exp(f3) + 0.01)
+df['y'] = rng.normal(loc=mu, scale=tau)
+
+
+mod = GauLS("y~C(x0)+s(x1, df=20, kind='cr')+s(x2, df=20, kind='cr')",
+            "y~1+s(x2, df=20, kind='cr')", df)
+mod.fit(opt_kws=dict(options=dict(verbose=3)))
+
+mod.plot_smooth_comp(mod.beta, single_fig=False)
+mod.plot_smooth_quantiles('x1', 'x2')
+mod.plot_smooth_quantiles('x2', 'x2', quantiles=[1, 5, 10, 15, 20, 25, 30, 
+                                                 35, 40, 45])
+
+np.allclose(np.array([9.363336,  9.186460, 12.551902]), mod.theta)
+
+print(mod.res)
+print(mod.res_smooths)
+print(mod.sumstats)
