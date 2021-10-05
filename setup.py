b6b57193e10dde3a827d1370431baeef45be7522
lukepinkel <lukepinkel@gmail.com>
2021-10-05 14:29:59 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  5 14:25:28 2021
+
+@author: lukepinkel
+"""
+
+import setuptools
+
+setuptools.setup(
+    name="pystatsm",
+    version="0.1.1",
+    url="https://github.com/lukepinkel/pystatsm",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
