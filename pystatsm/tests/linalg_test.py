160f408a5428d79d74cc7ef68d3642a9addea9ed
lukepinkel <lukepinkel@gmail.com>
2022-04-20 16:43:06 -0700

    Update linalg_test.py

--- pystatsm/tests/linalg_test.py
+++ pystatsm/tests/linalg_test.py
@@ -85,6 +85,79 @@ def test_vec():
     L2 = linalg.invech_chol(lv)
     
     assert(np.allclose(L1, L2))
+    
+def test_vec_alt():
+    n = 17
+    m = 11
+    p = 29
+    q = 19
+
+    x2 = np.arange(n*m).reshape(n, m)
+    y2 = linalg._vec(x2)
+
+    x3 = np.arange(n*m*p).reshape(p, n, m)
+    y3 = linalg._vec(x3)
+
+    x4 = np.arange(n*m*p*q).reshape(p, q, n, m)
+    y4 = linalg._vec(x4)
+
+
+    assert(np.allclose(linalg._invec(linalg._vec(x2), n, m), x2))
+    assert(np.allclose(linalg._vec(linalg._invec(y2, n, m)), y2))
+
+    assert(np.allclose(linalg._invec(linalg._vec(x3), n, m), x3))
+    assert(np.allclose(linalg._vec(linalg._invec(y3, n, m)), y3))
+
+    assert(np.allclose(linalg._invec(linalg._vec(x4), n, m), x4))
+    assert(np.allclose(linalg._vec(linalg._invec(y4, n, m)), y4))
+        
+    n = 7
+    m = 2
+    p = 3
+    q = 5
+    
+    a2 = np.arange(m*n).reshape(m, n)
+    a3 = np.arange(m*n*p).reshape(p, m, n)
+    a4 = np.arange(m*n*p*q).reshape(p, q, m, n)
+    
+    
+    x2 = np.einsum("kl,km->lm", a2, a2)
+    y2 = linalg._vech(x2)
+    
+    x3 = np.einsum("jkl,jkm->jlm", a3, a3)
+    y3 = linalg._vech(x3)
+    
+    x4 = np.einsum("ijkl,ijkm->ijlm", a4, a4)
+    y4 = linalg._vech(x4)
+    
+    assert(np.allclose(linalg._invech(linalg._vech(x2)), x2))
+    assert(np.allclose(linalg._vech(linalg._invech(y2)), y2))
+    
+    assert(np.allclose(linalg._invech(linalg._vech(x3)), x3))
+    assert(np.allclose(linalg._vech(linalg._invech(y3)), y3))
+    
+    assert(np.allclose(linalg._invech(linalg._vech(x4)), x4))
+    assert(np.allclose(linalg._vech(linalg._invech(y4)), y4))
+    
+    x2[..., np.diag_indices(n)[0], np.diag_indices(n)[1]] = 1
+    y2 =  linalg._vecl(x2)
+    x3[..., np.diag_indices(n)[0], np.diag_indices(n)[1]] = 1
+    y3 =  linalg._vecl(x3)
+    x4[..., np.diag_indices(n)[0], np.diag_indices(n)[1]] = 1
+    y4 =  linalg._vecl(x4)
+    
+    assert(np.allclose(linalg._invecl(linalg._vecl(x2)), x2))
+    assert(np.allclose(linalg._vecl(linalg._invecl(y2)), y2))
+    
+    assert(np.allclose(linalg._invecl(linalg._vecl(x3)), x3))
+    assert(np.allclose(linalg._vecl(linalg._invecl(y3)), y3))
+    
+    assert(np.allclose(linalg._invecl(linalg._vecl(x4)), x4))
+    assert(np.allclose(linalg._vecl(linalg._invecl(y4)), y4))
+
+
+
+
 
 
 
