e12095b2a0c126f263a5315b5fda5126df2811a8
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2022-08-05 20:11:30 -0700

    Update simulate_factor_model.py

--- pystatsm/pyfa/simulate_factor_model.py
+++ pystatsm/pyfa/simulate_factor_model.py
@@ -5,12 +5,17 @@ Created on Sat Apr  9 22:04:01 2022
 
 @author: lukepinkel
 """
-
+import tqdm
 import numpy as np
 import scipy as sp
 import scipy.linalg
 import pandas as pd
+from .efa import FactorAnalysis
+from .align_loadings import align_model_matrices 
 from ..utilities.random import r_lkj, exact_rmvnorm
+from ..utilities.func_utils import handle_default_kws
+from ..utilities.linalg_operations import vec, invecl, vecl
+from ..utilities.data_utils import _csd
 
 class FactorModelSim(object):
     
@@ -85,10 +90,75 @@ class FactorModelSim(object):
 
         
     
+
+def cov(arr):
+    arr = arr - np.mean(arr, axis=0)
+    S = np.dot(arr.T, arr) / len(arr)
+    return S
+
+class FactorEstimateSim(object):
     
-        
+    def __init__(self, p=9, m=3, sim_kws=None, cov_kws=None, data_kws=None):
+        sim_kws = handle_default_kws(sim_kws, {})
+        cov_kws = handle_default_kws(cov_kws, {})
+        data_kws = handle_default_kws(data_kws, dict(exact=False))
+            
+        model_sim = FactorModelSim(n_vars=p, n_facs=m, **sim_kws)
+        model_sim.simulate_cov(**cov_kws)
+        self.model_sim = model_sim
+        self.p, self.m = p, m
+    
+    def align_model(self, model):
+        L, L_se, Phi, Phi_se = model.L, model.L_se, model.Phi, model.Phi_se
+        L, L_se, Phi, Phi_se, _, _ = align_model_matrices(self.model_sim.L, L, Phi, 
+                                                    L_se, Phi_se)
+        Psi, Psi_se = np.diag(model.Psi), model.psi_se
+        if model.rotation_type == "oblique":
+            params = np.concatenate([vec(L), vecl(Phi), Psi])
+            params_se = np.concatenate([vec(L_se), vecl(Phi_se), Psi_se])
+        else:
+            params = np.concatenate([vec(L), Psi])
+            params_se = np.concatenate([vec(L_se), Psi_se])
+        return params, params_se
 
     
+    def simulate(self, n=100, n_obs=200, model_kws=None):
+        model_kws = handle_default_kws(model_kws, dict(n_factors=self.m, 
+                                                       rotation_method="varimax",
+                                                       rotation_type="oblique"))
+        pbar = tqdm.tqdm(total=n, smoothing=0.001)
+        Z, X = self.model_sim.simulate_data(n_obs=n_obs, exact=False)
+        model = FactorAnalysis(X, **model_kws)
+        model.fit()
+        model.rho_init = np.log(np.diag(self.model_sim.Psi))
+        params = np.zeros((n, len(model.res)))
+        params_se = np.zeros_like(params)
+        cov_mats = np.zeros((n, self.p, self.p))
+        for i in range(n):
+            Z, X = self.model_sim.simulate_data(n_obs=n_obs, exact=False)
+            model.S = cov(X)#model._process_data(X, None, X.shape[0])
+            cov_mats[i] = model.S.copy()
+            #model._init_psi()
+            model.fit()
+            params[i], params_se[i] = self.align_model(model)
+            pbar.update(1)
+        pbar.close()
+        if model.rotation_type == "oblique":
+            sim_pars = np.concatenate([vec(self.model_sim.L), vecl(self.model_sim.Phi), 
+                                       np.diag(self.model_sim.Psi)])
+        else:
+            sim_pars = np.concatenate([vec(self.model_sim.L), np.diag(self.model_sim.Psi)])
+        self.params, self.params_se =params, params_se
+        res = np.vstack((params.mean(axis=0),
+                         sim_pars,
+                         params.std(axis=0),
+                         params_se.mean(axis=0))).T
+        self.res = pd.DataFrame(res, index=model.res.index, columns=["Mean", "Generative Values", "SE", "SEM"])
+        self.res["ratio"] = self.res["SE"] / self.res["SEM"]
+        self.cov_mats = cov_mats
+
+            
+        
         
         
     
