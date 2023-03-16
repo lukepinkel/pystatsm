fix gaussian and inverse gaussian scale derivatives

pystatsm/pyglm/families.py
@@ -277,7 +277,7 @@ class Gaussian(ExponentialFamily):
         y, mu = self.cshape(y, mu)
         w = self.weights
         phi = np.exp(tau)
-        g = np.sum(w * np.power((y - mu), 2) / (2 * phi)) / 2
+        g = np.sum(w * np.power((y - mu), 2) / (2 * phi))
         return g
 
     def dvar_dmu(self, mu, scale=1.0):
@@ -381,7 +381,7 @@ class InverseGaussian(ExponentialFamily):
         y, mu = self.cshape(y, mu)
         w = self.weights
         phi = np.exp(tau)
-        g = np.sum(w * np.power((y - mu), 2) / (2 * phi * y * mu**2)) / 2
+        g = np.sum(w * np.power((y - mu), 2) / (2 * phi * y * mu**2))
         return g
 
     def dvar_dmu(self, mu, scale=1.0):

pystatsm/pyglm/linear_model.py
@@ -331,7 +331,8 @@ class GLM(RegressionMixin, LikelihoodModel):
         self.beta_init, self.phi_init = self.get_start_values()
         self.params_init = self.beta_init
 
-        if isinstance(self.f, (Binomial, Poisson)):
+        if isinstance(self.f, (Binomial, Poisson)) \
+            or self.f.name in ["Binomial", "Poisson"]:
             self.scale_estimator = 'fixed'
         else:
             self.scale_estimator = scale_estimator
@@ -431,7 +432,7 @@ class GLM(RegressionMixin, LikelihoodModel):
             phi = 1.0
         gw, hw = f.get_ghw(y, mu=mu, phi=phi)
         H = np.dot((X * hw.reshape(-1, 1)).T, X)
-        if isinstance(f, NegativeBinomial):
+        if isinstance(f, NegativeBinomial) or f.name=="NegativeBinomial":
             dbdt = -np.dot(X.T, phi * (y - mu) /
                            ((1 + phi * mu)**2 * f.dlink(mu)))
         else: