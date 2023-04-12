ac96b0737ef489964c231fba0e4d9a0508dd0384
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2023-04-11 17:49:58 -0700

    Update param_transforms_test.py

--- pystatsm/tests/param_transforms_test.py
+++ pystatsm/tests/param_transforms_test.py
@@ -10,26 +10,26 @@ from pystatsm.utilities.numerical_derivs import jac_approx
 from pystatsm.utilities.param_transforms import OrderedTransform
 
 x = sp.special.ndtri(np.linspace(0, 1, 6, endpoint=False)[1:])
-y = OrderedTransform._fwd(x, 5)
-x = OrderedTransform._rvs(y, 5)
+y = OrderedTransform._fwd(x)
+x = OrderedTransform._rvs(y)
 
 
-dx_dy = OrderedTransform._jac_rvs(y, 5)
-dy_dx = OrderedTransform._jac_fwd(x, 5)
+dx_dy = OrderedTransform._jac_rvs(y)
+dy_dx = OrderedTransform._jac_fwd(x)
 
-d2x_dy2 = OrderedTransform._hess_rvs(y, 5)
-d2y_dx2 = OrderedTransform._hess_fwd(x, 5)
+d2x_dy2 = OrderedTransform._hess_rvs(y)
+d2y_dx2 = OrderedTransform._hess_fwd(x)
 
 
-dx_dy_nm = jac_approx(lambda y: OrderedTransform._rvs(y.copy(), 5), y)
-dy_dx_nm = jac_approx(lambda x: OrderedTransform._fwd(x.copy(), 5), x)
+dx_dy_nm = jac_approx(lambda y: OrderedTransform._rvs(y.copy()), y)
+dy_dx_nm = jac_approx(lambda x: OrderedTransform._fwd(x.copy()), x)
 
 assert(np.allclose(dx_dy_nm, dx_dy))
 assert(np.allclose(dy_dx_nm, dy_dx))
 
 
-d2x_dy2_nm = jac_approx(lambda y: OrderedTransform._jac_rvs(y.copy(), 5), y)
-d2y_dx2_nm = jac_approx(lambda x: OrderedTransform._jac_fwd(x.copy(), 5), x)
+d2x_dy2_nm = jac_approx(lambda y: OrderedTransform._jac_rvs(y.copy()), y)
+d2y_dx2_nm = jac_approx(lambda x: OrderedTransform._jac_fwd(x.copy()), x)
 
 
 assert(np.allclose(d2x_dy2_nm, d2x_dy2))
