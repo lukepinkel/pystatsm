4a2ef1c8e78a489121a656d01d014888b3e8244e
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2023-06-06 19:12:05 -0700

    Update cov_derivatives.py

--- pystatsm/pysem/cov_derivatives.py
+++ pystatsm/pysem/cov_derivatives.py
@@ -11,49 +11,50 @@ import numpy as np
 from ..utilities.linalg_operations import _vech_nb
 
 @numba.jit(nopython=True)
-def _dsigma(dS, L, B, F, dA, r, c, mtype, ns):
+def _dsigma(dS, L, B, F, dA, r, c, mtype, ns, vhi):
     LB = L.dot(B)
-    BFBt = B.dot(F).dot(B.T)
+    BF = B.dot(F)
+    LBt = LB.T
+    BFBt = BF.dot(B.T)
     LBFBt = L.dot(BFBt)
     for i in range(ns):
         kind = mtype[i]
-        J = np.ascontiguousarray(dA[i, :r[i], :c[i]])
+        J = dA[i, :r[i], :c[i]]
         if kind == 0:
             J1 = LBFBt.dot(J.T)
-            tmp = _vech_nb((J1 + J1.T))
+            tmp = (J1 + J1.T)
         elif kind == 1:
-            J1 = J.dot(B.dot(F))
-            tmp =_vech_nb(LB.dot(J1+J1.T).dot(LB.T))
+            J1 = J.dot(BF)
+            tmp =LB.dot(J1+J1.T).dot(LBt)
         elif kind==2:
-            J1 = LB.dot(J).dot(LB.T)
-            tmp = _vech_nb(J1)
+            J1 = LB.dot(J).dot(LBt)
+            tmp = J1
         elif kind ==3:
-            tmp = _vech_nb(J)
-        dS[:, i] += tmp
+            tmp = J
+        dS[:, i] += tmp.T.flatten()[vhi]
     return dS
 
 @numba.jit(nopython=True)
-def _d2sigma(d2S, L, B, F, dA, r, c, ltr_inds, htype, ns2):
+def _d2sigma(d2S, L, B, F, dA, r, c, ltr_inds, htype, ns2, vhi):
     LB, BF = L.dot(B), B.dot(F)
     BFBt = BF.dot(B.T)
     for ij in range(ns2):
         i, j = ltr_inds[ij]
         kind = htype[ij]
-        Ji = np.ascontiguousarray(dA[i, :r[i], :c[i]])
-        Jj = np.ascontiguousarray(dA[j, :r[j], :c[j]])
+        Ji = dA[i, :r[i], :c[i]]
+        Jj = dA[j, :r[j], :c[j]]
         if kind == 1:
             tmp = (Ji.dot(BFBt).dot(Jj.T) + Jj.dot(BFBt).dot(Ji.T))
-            tmp = _vech_nb(tmp)
         elif kind == 2:
             BJj = B.T.dot(Jj.T)
             JiBF = Ji.dot(BF)
             C = JiBF + JiBF.T
             D = LB.dot(C).dot(BJj)
-            tmp = _vech_nb(D + D.T)
+            tmp = D + D.T
         elif kind == 3:
             JjB = Jj.dot(B)
             C = JjB.dot(Ji).dot(LB.T)
-            tmp = _vech_nb(C + C.T)
+            tmp = C + C.T
         elif kind == 4:
             C1 = Ji.dot(BF)
             C1 = C1 + C1.T
@@ -62,13 +63,13 @@ def _d2sigma(d2S, L, B, F, dA, r, c, ltr_inds, htype, ns2):
             t3 = C2.dot(C3.dot(F))
             t4 = BF.T.dot(C3.T).dot(C2.T)
             tmp = LB.dot(t1 + t1.T + t3 + t4).dot(LB.T)
-            tmp = _vech_nb(tmp)
+            tmp = tmp
         elif kind == 5:
             C = Jj.dot(B).dot(Ji)
-            tmp  = _vech_nb(LB.dot(C+C.T).dot(LB.T))
+            tmp  = LB.dot(C+C.T).dot(LB.T)
         else:
             continue
-        d2S[:, i, j] += tmp
+        d2S[:, i, j] += tmp.T.flatten()[vhi]
         d2S[:, j, i] = d2S[:, i, j]
     return d2S
     
