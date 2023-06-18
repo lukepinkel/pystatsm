ef28b7e6f03539e8958b8880b6d8b481d936b560
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2023-06-18 04:52:35 -0700

    Update formula_parser.py

--- pystatsm/pysem/formula_parser.py
+++ pystatsm/pysem/formula_parser.py
@@ -11,13 +11,38 @@ import numpy as np
 import pandas as pd
 
 class FormulaParser:
+    """
+    A class to parse a set of formulas and extract variables and parameters from the formulas.
+    """
     def __init__(self, formulas):
+        """
+        Initialize the FormulaParser with a set of formulas.
+        
+        Parameters
+        ----------
+        formulas : str
+            A string of formulas to be parsed, separated by newlines.
+        """
         self._formulas = formulas
         self._param_list = self.parse_formula(formulas)
         self._param_df = pd.DataFrame(self._param_list)
 
     @staticmethod  
     def parse_formula(formulas):
+        """
+        Parse a set of formulas (in a single string separated by newlines), 
+        extracting all variables and parameters.
+    
+        Parameters
+        ----------
+        formulas : str
+            A string of formulas to be parsed, separated by newlines.
+    
+        Returns
+        ----------
+        parameters : list of dict
+            A list of dictionaries, each representing a parameter extracted from the formulas.
+        """
         formulas = re.sub(r'\s*#.*', '', formulas)
         parameters= []
         equations = formulas.strip().split('\n')
@@ -28,6 +53,25 @@ class FormulaParser:
         
     @staticmethod  
     def get_var_pair(ls, rs, rel):
+        """
+        Extracts a variable pair and relationship from a formula.
+    
+        Parameters
+        ----------
+        ls : str
+            The left-hand side of the equation.
+    
+        rs : str
+            The right-hand side of the equation.
+    
+        rel : str
+            The relationship operator in the equation (one of "=~", "~~", or "~").
+    
+        Returns
+        ----------
+        row : dict
+            A dictionary representing the extracted variable pair and relationship.
+        """
         comps = rs.strip().split('*')
         ls = ls.strip()
         if len(comps) > 1:
@@ -54,6 +98,22 @@ class FormulaParser:
      
     @staticmethod        
     def unpack_equation(eq, parameters):
+        """
+        Unpacks an equation into component variable pairs and relationships.
+        
+        Parameters
+        ----------
+        eq : str
+            The equation to be unpacked.
+        
+        parameters : list of dict
+            A list of dictionaries, each representing a parameter.
+        
+        Returns
+        ----------
+        parameters : list of dict
+            The updated list of dictionaries representing parameters, after unpacking the equation.
+        """
         if "=~" in eq:
             rel = "=~"
         elif "~~" in eq:
@@ -69,6 +129,19 @@ class FormulaParser:
     
     @staticmethod
     def classify_variables(param_df):
+        """
+        Classifies variables into several categories based on their role in the formulas.
+    
+        Parameters
+        ----------
+        param_df : pandas.DataFrame
+            DataFrame where each row represents a parameter in the formula.
+    
+        Returns
+        ----------
+        names : dict
+            A dictionary where keys are categories and values are sets of variable names in each category.
+        """
         one_set = set(["1"])
         measurement_mask = param_df["rel"] == "=~"
         regressions_mask  = ((param_df["rel"] == "~") & (param_df["rhs"]!="1"))
