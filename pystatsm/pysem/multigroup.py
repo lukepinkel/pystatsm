5092b091e2039c2c1ccad58ceaf31e09960450ac
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2023-06-18 04:52:20 -0700

    Update multigroup.py

--- pystatsm/pysem/multigroup.py
+++ pystatsm/pysem/multigroup.py
@@ -22,11 +22,7 @@ def _sparse_post_mult(A, S):
     prod = prod.T
     return prod
 
-
-
-
 pd.set_option("mode.chained_assignment", None)
-
 
 class SimpleEqualityConstraint:
     def __init__(self, indicators):
@@ -64,16 +60,37 @@ def equality_constraint_mat(unique_locs):
     arr = sp.sparse.csc_matrix((data, (row, col)), shape=(m, n))
     return arr
 
-
-
-
-
-
-
-
 class ModelSpecification(object):
+    """
+    A class that manages the setup of model specifications, particularly for handling equality constraints.
+    
+    Methods
+    -------
+    __init__(formula, data, group_col)
+        Initialize the ModelSpecification object, assigns group IDs, and calls create_group_tables method.
+    
+    create_group_tables(formula, data, group_col)
+        Creates group tables based on the formula and data provided, creates ParameterTable objects, and prepares model matrices.
+    
+    add_matrix_equalities(shared)
+        Manages parameter equality constraints across matrices in the model.
+    """
+
     
     def __init__(self, formula, data, group_col):
+        """
+        Initializes the ModelSpecification object, assigns group IDs based on unique values in group_col, 
+        and calls create_group_tables method with the provided formula, data, and group column.
+
+        Parameters
+        ----------
+        formula : str
+            A string that specifies the model formula.
+        data : pandas.DataFrame
+            The data to be used in the model.
+        group_col : str
+            The name of the column in 'data' that contains the group identifiers.
+        """
         group_ids = np.unique(data[group_col])
         n_groups = len(group_ids)
         data[group_col] = data[group_col].replace(dict(zip(group_ids, np.arange(n_groups)))) 
@@ -81,6 +98,19 @@ class ModelSpecification(object):
         self.create_group_tables(formula, data, group_col)
         
     def create_group_tables(self, formula, data, group_col):
+        """
+        Processes the data based on the provided formula and group column, and prepares necessary structures for the model.
+        Creates ParameterTable objects, and constructs model matrices.
+
+        Parameters
+        ----------
+        formula : str
+            A string that specifies the model formula.
+        data : pandas.DataFrame
+            The data to be used in the model.
+        group_col : str
+            The name of the column in 'data' that contains the group identifiers.
+        """
         self.ptables, self.ftables , self.ptable_objects= {}, {}, {}
         self.p_templates, self.indexers = {}, {}
         self.sample_covs = {}
@@ -120,27 +150,66 @@ class ModelSpecification(object):
         self.ftable = ftable
         
     def add_matrix_equalities(self, shared):
+        """
+        Manages parameter equality constraints across matrices in the model.
+        Generates unique labels for each parameter, flags duplicate parameters
+        across different groups, creates a transformation matrix, and a dictionary 
+        to map from  the free parameters of each group to the full set of 
+        free parameters.
+
+        Parameters
+        ----------
+        shared : list of bool
+            A list of booleans indicating whether each matrix in the model is
+            shared across groups or not.
+        """
+        # This section of the code is focused on label generation for each parameter 
+        # defined in the formula and tracking potential parameter duplications. 
+        
+        # It starts by copying the "mod" column into a new column called "label", 
+        # which will be used to store the unique identifiers for each row (parameter). 
+        # A "duplicate" column is also created to keep track of whether a parameter 
+        # is duplicated across different groups.
+        
+        # The code then loops over all possible types of matrices (from 0 to 5), 
+        # creating a unique label for each parameter. This is achieved by joining 
+        # strings from various columns. If a matrix type is flagged as "shared", 
+        # the label doesn't include the group identifier (level_0), allowing parameters 
+        # to be shared across groups. Otherwise, the group identifier is included in the 
+        # label to ensure it's unique across all groups.
+        
+        # After label creation, the code iterates over each unique group, and for each 
+        # matrix type, if it's shared, it marks parameters as duplicates if they are not 
+        # in the first group (i.e., if level_0 > 0). 
+        
+        # Finally, the function "indexing_utils.unique" is called on the "label" column 
+        # to obtain the unique labels, their inverse mapping, and indices, which will 
+        # be useful for further processing.
         self.shared = shared
         ftable = self.ftable
         ix = ftable["mod"].isnull()
-        ftable["label"]= ftable["mod"].copy()
-        ftable["duplicate"] = False
+        ftable["label"]= ftable["mod"].copy() 
+        # ftable["duplicate"] = False
         for i in range(6):
+            #Identify parameters (in matrix i) without labels already specified particular in the formula
             ix = ftable["mat"]==i
             not_null = ~ftable.loc[ix, "label"].isnull()
             ix1 = ix & not_null
+            #Either add a label that will be unique across groups by adding group id or shared across groups
             if self.shared[i]:
                 label = ftable[["lhs", "rel","rhs"]].astype(str).agg(' '.join, axis=1)
             else:
                 label = ftable[["level_0", "lhs", "rel","rhs"]].astype(str).agg(' '.join, axis=1)
                 ftable.loc[ix1, "label"] = ftable.loc[ix1, ["level_0", "label"]].astype(str).agg(' '.join, axis=1)
             ftable.loc[ix, "label"] = ftable.loc[ix, "label"].fillna(label)
-        for i in np.unique(ftable["level_0"]):
-            group_ix = ftable["level_0"] == i
-            for j in range(6):
-                mat_ix = ftable["mat"]==j
-                if self.shared[j]:
-                    ftable.loc[mat_ix & group_ix, "duplicate"] = i>0
+        
+        
+        # for i in np.unique(ftable["level_0"]):
+        #     group_ix = ftable["level_0"] == i
+        #     for j in range(6):
+        #         mat_ix = ftable["mat"]==j
+        #         if self.shared[j]:
+        #             ftable.loc[mat_ix & group_ix, "duplicate"] = i>0
             
         unique_values, inverse_mapping, indices = indexing_utils.unique(ftable["label"])
         unique_labels = pd.Series(unique_values)
