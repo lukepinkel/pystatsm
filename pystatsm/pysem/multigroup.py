cfc7e68a589dc27cb79a3532508a3865de1424c5
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2023-06-22 03:33:17 -0700

    Update multigroup.py

--- pystatsm/pysem/multigroup.py
+++ pystatsm/pysem/multigroup.py
@@ -12,7 +12,7 @@ import pandas as pd
 
 from ..utilities.linalg_operations import  _vech, _vec, _invec, _invech
 from ..utilities.func_utils import handle_default_kws, triangular_number
-from .derivatives import _dloglike_mu, _d2loglike_mu0, _d2loglike_mu1, _d2loglike_mu2 #from .cov_derivatives import _d2sigma, _dsigma, _dloglike, _d2loglike
+from .derivatives import _dloglike_mu, _d2loglike_mu0, _d2loglike_mu1, _d2loglike_mu2, _dsigma_mu, _d2sigma_mu #from .cov_derivatives import _d2sigma, _dsigma, _dloglike, _d2loglike
 from .param_table import ParameterTable
 from .model_data import ModelData
 from ..utilities import indexing_utils
@@ -211,14 +211,6 @@ class ModelSpecification(object):
                 ftable.loc[ix1, "label"] = ftable.loc[ix1, ["level_0", "label"]].astype(str).agg(' '.join, axis=1)
             ftable.loc[ix, "label"] = ftable.loc[ix, "label"].fillna(label)
         
-        
-        # for i in np.unique(ftable["level_0"]):
-        #     group_ix = ftable["level_0"] == i
-        #     for j in range(6):
-        #         mat_ix = ftable["mat"]==j
-        #         if self.shared[j]:
-        #             ftable.loc[mat_ix & group_ix, "duplicate"] = i>0
-            
         unique_values, inverse_mapping, indices = indexing_utils.unique(ftable["label"])
         unique_labels = pd.Series(unique_values)
         label_to_ind = pd.Series(unique_labels.index, index=unique_labels.values)
@@ -274,7 +266,7 @@ class SEM:
         self.theta = self.mspec.free_to_theta.dot(self.free)
         self.n_par = len(self.p_templates[0])
         self.ll_const =  1.8378770664093453 * self.p
-        self.n_groups =self. mspec.n_groups
+        self.n_groups =self.mspec.n_groups
         self.indexers = self.mspec.indexers
         self.gsizes = self.model_data.data_df["group"].value_counts().values
         self.gweights = self.gsizes / np.sum(self.gsizes)
@@ -282,8 +274,9 @@ class SEM:
     def make_derivative_matrices(self):
         self.indexer.create_derivative_arrays([(0, 0), (1, 0), (2, 0), (1 ,1), (2, 1), (5, 0), (5, 1)])
         self.dA = self.indexer.dA
-        self.dSm = np.zeros((self.p2+self.p, self.nf))
-        self.d2Sm = np.zeros((self.p2+self.p, self.nf, self.nf))
+        self.n_group_theta = self.mspec.free_to_theta.shape[0]
+        self.dSm = np.zeros((self.mspec.n_groups, self.p2+self.p, self.nf))
+        self.d2Sm = np.zeros((self.mspec.n_groups, self.p2+self.p, self.nf, self.nf))
         self.m_size = self.indexer.block_sizes                    
         self.m_kind = self.indexer.block_indices                 
         self.d2_kind = self.indexer.block_pair_types             
@@ -291,8 +284,6 @@ class SEM:
         self.J_theta = sp.sparse.csc_array(
             (np.ones(self.nf), (np.arange(self.nf), self.indexer.unique_locs)), 
              shape=(self.nf, self.nt))
-        self.dSm = np.zeros((self.p2+self.p, self.nf))
-        self.d2Sm = np.zeros((self.p2+self.p, self.nf, self.nf))
         s, r = np.triu_indices(self.p, k=0)
         self._vech_inds = r+s*self.p
         self.unique_locs = self.indexer.unique_locs
@@ -307,7 +298,9 @@ class SEM:
                               n=self.nf,  vech_inds=self._vech_inds)
             
 
-     
+    def free_to_theta(self, free):
+         theta = free[self._first_locs]
+         return theta
     
     def func(self, theta, per_group=False):
         free = self._theta_to_free(theta)
@@ -358,7 +351,6 @@ class SEM:
             R = self.mspec.sample_covs[i] - Sigma
             Sinv = np.linalg.inv(Sigma)
             VRV = Sinv.dot(R).dot(Sinv)
-            vecVRV = _vec(VRV)
             rtV = (self.mspec.sample_means[i].flatten() - mu).dot(Sinv)
             gi = np.zeros(self.nf)
             kws = self._grad_kws
@@ -522,7 +514,6 @@ class SEM:
             R = self.mspec.sample_covs[i] - Sigma
             Sinv = np.linalg.inv(Sigma)
             VRV = Sinv.dot(R).dot(Sinv)
-            vecVRV = _vec(VRV)
             rtV = (self.mspec.sample_means[i].flatten() - mu).dot(Sinv)
             gi = np.zeros(self.nf)
             kws = self._grad_kws
@@ -532,6 +523,35 @@ class SEM:
             else:
                 g = g + self.mspec.free_to_group_freet[i].dot(gi)
         return g
+    
+    def dsigma_mu(self, theta):
+        free = self._theta_to_free(theta)
+        dSm = self.dSm.copy()
+        for i in range(self.n_groups):
+            group_free = self._free_to_group_free(free, i)
+            par = self._free_to_par(group_free, i)
+            L, B, F, P, a, b = self._par_to_model_mats(par, i)
+            B = np.linalg.inv(np.eye(B.shape[0])-B)
+            kws =  dict(dA=self.dA, m_size=self.m_size, m_type=self.m_kind,
+                        vind=self._vech_inds, n=self.nf, p2=self.p2)
+            a, b = a.flatten(), b.flatten()
+            dSm[i]  = _dsigma_mu(dSm[i], L, B, F, b, a,**kws)
+        return dSm
+
+    def d2sigma_mu(self, theta):
+        free = self._theta_to_free(theta)
+        d2Sm = self.d2Sm.copy()
+        for i in range(self.n_groups):
+            group_free = self._free_to_group_free(free, i)
+            par = self._free_to_par(group_free, i)
+            L, B, F, P, a, b = self._par_to_model_mats(par, i)
+            B = np.linalg.inv(np.eye(B.shape[0])-B)
+            a, b = a.flatten(), b.flatten()
+            kws =  dict(dA=self.dA, m_size=self.m_size,  m_type=self.d2_kind, 
+                        d2_inds=self.d2_inds, vind=self._vech_inds, n=self.nf2, 
+                        p2=self.p2)
+            d2Sm[i] = _d2sigma_mu(d2Sm[i], L, B, F, b, a,**kws)
+        return d2Sm
             
     def _par_to_model_mats(self, par, i):
         slices = self.indexers[i].slices
@@ -558,7 +578,7 @@ class SEM:
 
         
     def _theta_to_free(self, theta):
-        free = self.mspec.theta_to_free.dot(theta)
+        free = theta[self.mspec._unique_locs]
         return free
 
     def _implied_cov_mean(self, L, B, F, P, a, b):
@@ -570,36 +590,32 @@ class SEM:
     
     def implied_cov_mean(self, theta):
         free = self._theta_to_free(theta)
-        Sigmas = {}
-        mus = {}
+        Sigma = np.zeros((self.n_groups, self.p, self.p))
+        mu = np.zeros((self.n_groups, self.p))
         for i in range(self.n_groups):
             group_free = self._free_to_group_free(free, i)
             par = self._free_to_par(group_free, i)
             mats = self._par_to_model_mats(par, i)
-            Sigmas[i], mus[i] = self._implied_cov_mean(*mats)
-        return Sigmas, mus
+            Sigma[i], mu[i] = self._implied_cov_mean(*mats)
+        return Sigma, mu
     
-    def implied_sample_stats(self, free):
-        L, B, F, P, a, b = self.free_to_model_mats(free)
-        B = np.linalg.inv(np.eye(B.shape[0])-B)
-        LB = np.dot(L, B)
-        Sigma = LB.dot(F).dot(LB.T) + P
-        mu = (a+LB.dot(b.T).T).reshape(-1)
-        s, mu = _vech(Sigma), mu.flatten()
-        sm = np.concatenate([s, mu])
-        return sm
-    
-    # def loglike(self, free, reduce=True):
-    #     Sigma, mu = self.implied_cov_mean(free)
-    #     L = sp.linalg.cholesky(Sigma) #np.linalg.cholesky(Sigma)
-    #     Y = self.model_data.data - mu
-    #     Z = sp.linalg.solve_triangular(L, Y.T, trans=1).T #np.dot(X, np.linalg.inv(L.T))
-    #     t1 = 2.0 * np.log(np.diag(L)).sum() 
-    #     t2 = np.sum(Z**2, axis=1)
-    #     ll = (t1 + t2 + self.ll_const) / 2
-    #     if reduce:
-    #         ll = np.sum(ll)
-    #     return ll
+    def implied_sample_stats(self, theta):
+        free = self._theta_to_free(theta)
+        Sigmamu = np.zeros((self.n_groups, self.p2+self.p))
+        if np.iscomplexobj(theta):
+            Sigmamu = Sigmamu.astype(complex)
+        for i in range(self.n_groups):
+            group_free = self._free_to_group_free(free, i)
+            par = self._free_to_par(group_free, i)
+            L, B, F, P, a, b = self._par_to_model_mats(par, i)
+            B = np.linalg.inv(np.eye(B.shape[0])-B)
+            LB = np.dot(L, B)
+            Sigma = LB.dot(F).dot(LB.T) + P
+            mu = (a+LB.dot(b.T).T).reshape(-1)
+            s, mu = _vech(Sigma), mu.flatten()
+            Sigmamu[i, :self.p2] = s
+            Sigmamu[i, self.p2:] = mu
+        return Sigmamu
     
     def loglike(self, theta, level="sample"):
         free = self._theta_to_free(theta)
