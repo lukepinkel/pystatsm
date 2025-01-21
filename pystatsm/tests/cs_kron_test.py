6c8c585c3fb9da82446ab2fd6f84501764576ec1
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2025-01-20 18:17:26 -0800

    Update cs_kron_test.py

--- pystatsm/tests/cs_kron_test.py
+++ pystatsm/tests/cs_kron_test.py
@@ -1,7 +1,7 @@
 import timeit
 import numpy as np
 import scipy as sp
-from pystatsm.utilities.cs_kron import sparse_kron, sparse_kron_jit, get_csc_eq
+from pystatsm.utilities.python_wrappers import sparse_kron, csc_eq
 rng = np.random.default_rng(123)
 
 
@@ -11,7 +11,5 @@ B = sp.sparse.random(100, 100, density=0.1, format="csc")
 
 C1 = sparse_kron(A, B)
 C2 = sp.sparse.kron(A, B, format='csc')
-C3 = sparse_kron_jit(A, B)
-assert(get_csc_eq(C1, C2))
-assert(get_csc_eq(C2, C3))
-assert(get_csc_eq(C1, C3))
+
+assert(csc_eq(C1, C2))
