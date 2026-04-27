10ea6bd0f764a38483b921d8a824dd043101c80c
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2026-04-27 15:35:48 -0700

    Create inference.py

---
+++ pystatsm/pyfa2/inference.py
@@ -0,0 +1,44 @@
+import numpy as np
+from ..utilities.linalg_operations import _vech
+
+def augmented_hessian(H, dCdTheta):
+    nt = H.shape[0]
+    n_c = dCdTheta.shape[0]
+    A = np.zeros((nt + n_c, nt + n_c))
+    A[:nt, :nt] = H
+    A[:nt, nt:] = dCdTheta.T
+    A[nt:, :nt] = dCdTheta
+    return A
+
+
+def bread(H, dCdTheta, free_mask):
+    nt = H.shape[0]
+    n_c = dCdTheta.shape[0]
+    A = augmented_hessian(H, dCdTheta)
+    free = np.r_[np.where(free_mask)[0], nt + np.arange(n_c)]
+    Ainv = np.linalg.inv(A[np.ix_(free, free)])
+    n_free = free.size - n_c
+    return Ainv[:n_free, :n_free]
+
+
+def param_cov(H, dCdTheta, free_mask, n_obs):
+    return bread(H, dCdTheta, free_mask) * (2.0 / n_obs)
+
+
+def sandwich_cov(H, dCdTheta, J, free_mask):
+    b = bread(H, dCdTheta, free_mask)
+    free_idx = np.where(free_mask)[0]
+    return np.matmul(np.matmul(b, J[np.ix_(free_idx, free_idx)]), b.T)
+
+
+def se_from_cov(V):
+    return np.sqrt(np.diag(V))
+
+
+def empirical_cov_vech_S(X):
+    n = X.shape[0]
+    Xc = X - X.mean(axis=0, keepdims=True)
+    outer = np.einsum('ij,ik->ijk', Xc, Xc)
+    vechs = _vech(outer)
+    centered = vechs - vechs.mean(axis=0, keepdims=True)
+    return np.matmul(centered.T, centered) / (n * (n - 1))
