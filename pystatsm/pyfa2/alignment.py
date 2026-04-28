1b98df9534a452318805795503f8489fe026fc7b
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2026-04-28 14:32:52 -0700

    Update alignment.py

--- pystatsm/pyfa2/alignment.py
+++ pystatsm/pyfa2/alignment.py
@@ -5,9 +5,9 @@ from scipy.optimize import linear_sum_assignment
 def align(L_est, L_target, normalize=False):
     M = np.matmul(L_target.T, L_est)
     if normalize:
-        nt = np.linalg.norm(L_target, axis=0)
-        ne = np.linalg.norm(L_est, axis=0)
-        M = M / (nt[:, None] * ne[None, :] + 1e-30)
+        nt = np.linalg.norm(L_target, axis=0).reshape(-1, 1)
+        ne = np.linalg.norm(L_est, axis=0).reshape(1, -1)
+        M = M / (nt * ne + 1e-30)
     _, perm = linear_sum_assignment(-np.abs(M))
     signs = np.sign(np.sum(L_target * L_est[:, perm], axis=0))
     signs[signs == 0] = 1.0
@@ -19,7 +19,9 @@ def apply(L, perm, signs):
 
 
 def apply_phi(Phi, perm, signs):
-    return signs[:, None] * Phi[np.ix_(perm, perm)] * signs[None, :]
+    s_col = signs.reshape(-1, 1)
+    s_row = signs.reshape(1, -1)
+    return s_col * Phi[np.ix_(perm, perm)] * s_row
 
 
 def apply_se(L_se, perm):
