98bc698507dbad18917a158f38daa634a62750f4
lukepinkel <46092990+lukepinkel@users.noreply.github.com>
2026-05-03 00:30:22 -0700

    Create __init__.py

---
+++ pystatsm/utilities/complexdesign/__init__.py
@@ -0,0 +1,4 @@
+from .sample_design import SampleDesign, IndexLayout, design_sandwich
+from .population import (Population, LinearPopulation, BinomialPopulation)
+from .replication import (FitResult, WLSAdapter, GLMAdapter,
+                          ReplicationResult, replicate)
